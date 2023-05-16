f2bd40de484aff28bca0ab8a209973609e04cbbf
Michael X. Grey <mxgrey@intrinsic.ai>
2023-05-16 03:30:37 +0000

    Build the grid example
    Signed-off-by: Michael X. Grey <mxgrey@intrinsic.ai>

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -3,7 +3,7 @@ set -o verbose
 set -o errexit
 export CARGO_PROFILE_RELEASE_LTO=true
 export CARGO_PROFILE_RELEASE_OPT_LEVEL=z
-cargo build --target wasm32-unknown-unknown --release
+cargo build --example grid --target wasm32-unknown-unknown --release
 RUST_BACKTRACE=full wasm-bindgen --target web --out-dir web target/wasm32-unknown-unknown/release/examples/grid.wasm
 cd web
 wasm-opt -Oz -o libgrid_bg_optimized.wasm libgrid_bg.wasm
