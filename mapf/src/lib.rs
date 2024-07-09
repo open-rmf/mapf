d65f04a0819dc602676a68a902f121a9e8b19b28
Michael X. Grey <grey@openrobotics.org>
2024-07-09 12:34:37 +0800

    Fix style
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- mapf/src/lib.rs
+++ mapf/src/lib.rs
@@ -20,12 +20,11 @@
     impl_trait_in_assoc_type,
     result_flattening
 )]
-
 // TODO(@mxgrey): Eliminate the need for this by reducing the complexity of
 // struct names, e.g. the typename for the complex chain that implements Lifted
 // in activity.rs. This will probably go hand-in-hand with removing the need for
 // nightly features, and improve compile times all at once.
-#![type_length_limit="157633981"]
+#![type_length_limit = "157633981"]
 
 pub mod domain;
 
