7b3755b738451f2382cedfbe36e28c45e5b6f338
Arjo Chakravarty <arjoc@intrinsic.ai>
2026-06-18 03:00:58 +0000

    docs: Add movingai related documentation
    Signed-off-by: Arjo Chakravarty <arjoc@intrinsic.ai>

---
+++ mapf-bench/README.md
@@ -0,0 +1,5 @@
+# `mapf-bench`
+
+The sole purpose of this crate is to run benchmarks using the `mapf` library. Currently the benchmarks we
+run are run against [moving AI](https://www.movingai.com/benchmarks/mapf/)'s benchmarks. This benchmark is often
+seen as the defacto "mapf" banchmark. This crate contains a parser to load the benchmark files into memory. 
--- mapf-bench/src/movingai.rs
+++ mapf-bench/src/movingai.rs
@@ -1,8 +1,32 @@
+/*
+ * Copyright (C) 2026 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+//! This module contains a simple parser for the movingai benchmarks.
+//! This benchmark is widely seen as the de-facto mapf benchmark.
+//!
+//! The benchmarks consist of *.map files and *.scen files. The *.map
+//! files are occupancy grids where the *.scen files contain
+//! exact mapf scenarios.
+
+use anyhow::Result;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
-use anyhow::Result;
-use std::collections::HashMap;
 
 use mapf::negotiation::{Agent, Scenario};
 use std::collections::BTreeMap;
@@ -44,7 +68,11 @@ impl Map {
             }
         }
 
-        Ok(Map { width, height, grid })
+        Ok(Map {
+            width,
+            height,
+            grid,
+        })
     }
 
     pub fn to_occupancy_map(&self) -> HashMap<i64, Vec<i64>> {
